111a7ff5c2b70fc9fd4ed4a5b84a3fbdf6eb3a4f
Gustavo <github.com/Gus96costa>
2026-04-23 20:14:18 +0200

    chore: migração do Tailwind CDN para CLI (Otimização Lighthouse)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-tailwind.config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html", "./js/**/*.js"],
     theme: {
         extend: {
             fontFamily: {
